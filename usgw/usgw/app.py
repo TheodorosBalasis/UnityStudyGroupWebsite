1d34ea93db2a58914ca26073e3370a24280cddb1
Theodoros Balasis <theodorosbalasis@gmail.com>
2018-08-15 17:09:22 +0300

    Move routes to app.

--- usgw/usgw/app.py
+++ usgw/usgw/app.py
@@ -1,13 +1,45 @@
 from flask import render_template as render
+import flask
+import os
+
+from pymongo.collection import Collection
+
 from usgw.config import Config
 from usgw.db import get_db
-import flask, os
+from usgw.models.Resource import Resource
 
 config = Config()
-app    = flask.Flask(__name__)
+app = flask.Flask(__name__)
 app.secret_key = config['SECRET']
+
 
 @app.route('/')
 def index():
     db = get_db()
     return render('landing.html')
+
+
+@app.route('/join')
+def join():
+    return render('join.html')
+
+
+def get_resources():
+    # type: () -> Collection
+    return db['resources']
+
+
+def get_resource(id):
+    # type: (str) -> Resource
+    resources_collection = get_resources()
+    resource = resources_collection.find_one({"uuid": id})
+    resource = Resource.from_json(json)
+    return resource
+
+
+def post_resource():
+    pass
+
+
+def delete_resource():
+    pass
