0a25915db58ff301f42f7783002f3aa59990a523
Theodoros Balasis <theodorosbalasis@gmail.com>
2018-08-16 21:48:51 +0300

    Formatting util.py

--- usgw/usgw/util.py
+++ usgw/usgw/util.py
@@ -1,10 +1,12 @@
 import sys
 
+
 def trim(s):
     try:
         return ' '.join(s.split())
     except:
         return s
+
 
 def atoi(s):
     # convert object s to int without exception
@@ -13,13 +15,15 @@ def atoi(s):
     except:
         return 0
 
+
 def atob(s):
     # convert object s to bool without exception
     return bool(atoi(s))
+
 
 def eprint(m):
     try:
         m = str(m)
     except:
         m = 'ERR: bad conversion'
-    sys.stderr.write('\n%s\n\n'%(m,))
+    sys.stderr.write('\n%s\n\n' % (m,))
