f77038836f93b9a295dfe1e2f2fdc5561e0a5218
Theodoros Balasis <theodorosbalasis@gmail.com>
2018-08-16 18:40:14 +0300

    Add routing and route stubs for resources.

--- usgw/usgw/app.py
+++ usgw/usgw/app.py
@@ -1,4 +1,5 @@
 from flask import render_template as render
+from flask import request
 import flask
 import os
 
@@ -19,9 +20,13 @@ def index():
     return render('landing.html')
 
 
-@app.route('/resources')
+@app.route('/resources', methods=['GET', 'POST'])
 def resources():
-    return render('resources.html')
+    if request.method is 'GET':
+        return render('resources.html')
+    elif request.method is 'POST':
+        # Authentication stuff here
+        post_resource(request.form)
 
 
 @app.route('/contact')
@@ -34,24 +39,35 @@ def projects():
     return render('projects.html')
 
 
-@app.route('/resources/<uuid:id>')
+@app.route('/resources/<uuid:id>', methods=['GET', 'DELETE'])
 def get_resource_by_id(id):
     # type: (str) -> Resource
+    if request.method is 'GET':
+        return get_resource(id)
+    elif request.method is 'DELETE':
+        # Authentication stuff here
+        delete_resource(id)
+
+
+def get_resource(id):
+    # type: (str) -> Resource
     resources_collection = get_resources()
-    resource = resources_collection.find_one({"uuid": id})
-    resource = Resource.from_json(json)
+    resource = resources_collection.find_one({"_id": id})
+    resource = Resource.from_json(resource)
     return resource
 
 
-def post_resource():
+def post_resource(request_form):
+    # type: (dict) -> None
     pass
 
 
-def delete_resource():
-    pass
+def delete_resource(id):
+    # type: (str) -> None
+    resources = get_resources()
+    resources.delete_one({'_id': id})
 
 
 def get_resources():
     # type: () -> Collection
     return db['resources']
-
