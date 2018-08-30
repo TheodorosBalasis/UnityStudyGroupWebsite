22c5a92a26c9b5dec67444ad3a48bd8a273e3402
Theodoros Balasis <theodorosbalasis@gmail.com>
2018-08-30 14:23:32 +0300

    Add function to convert a dictionary to an arbitrary heap type.

--- usgw/usgw/models/ModelUtilities.py
+++ usgw/usgw/models/ModelUtilities.py
@@ -2,8 +2,6 @@ import json
 import inspect
 from flask import jsonify
 
-from usgw.models.Resource import Resource
-
 
 def to_json_response(model):
     return jsonify(json.dumps(model.__dict__))
@@ -16,13 +14,20 @@ def from_dict(dictionary, target_type):
         raise TypeError('Target type argument is not a type.')
     fields = None
     try:
-        fields = inspect.getargspec(target_type.__init__)
+        fields = inspect.getargspec(target_type.__init__).args
     except AttributeError:
         raise AttributeError('Type ' + target_type.__name__ + ' has no constructor.')
     fields = fields[1:len(fields)]
     for key in dictionary:
         if key not in fields:
             raise ValueError('Invalid field ' + str(key))
-    new_object = type('', (), dictionary)
+        elif type(key) != str and type(key) != unicode:
+            raise ValueError('Dictionary keys must be strings.')
+    new_object = type('temp', (object,), {})()
     new_object.__class__ = target_type
+    for key in dictionary:
+        setattr(new_object, key, dictionary[key])
+    type_methods = inspect.getmembers(type, predicate=inspect.ismethod)
+    for method in type_methods:
+        setattr(new_object, method[0], method[1])
     return new_object
