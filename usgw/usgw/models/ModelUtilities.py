cb8956e8d851bcf23ffefdca85092baef8e62309
Theodoros Balasis <theodorosbalasis@gmail.com>
2018-08-31 12:36:28 +0300

    Implement type-agnostic JSON deserialization function.

--- usgw/usgw/models/ModelUtilities.py
+++ usgw/usgw/models/ModelUtilities.py
@@ -10,7 +10,7 @@ def to_json_response(model):
 def from_dict(dictionary, target_type):
     if type(dictionary) != dict:
         raise TypeError('Dictionary argument is not a dictionary.')
-    if type(type) != type:
+    if type(target_type) != type:
         raise TypeError('Target type argument is not a type.')
     fields = None
     try:
@@ -31,3 +31,16 @@ def from_dict(dictionary, target_type):
     for method in type_methods:
         setattr(new_object, method[0], method[1])
     return new_object
+
+
+def from_json(json_input, target_type):
+    if type(json_input) != str and type(json_input) != unicode:
+        raise TypeError('JSON argument needs to be a string.')
+    if type(target_type) != type:
+        raise TypeError('Target type argument is not a type.')
+    dictionary = None
+    try:
+        dictionary = json.loads(json_input)
+    except ValueError:
+        raise ValueError('JSON input argument is an invalid JSON string.')
+    return from_dict(dictionary, target_type)
