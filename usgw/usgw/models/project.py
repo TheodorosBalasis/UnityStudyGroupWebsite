d9bc02d3caa2a18bbf75514e7a2c57d34a562e32
Theodoros Balasis <theodorosbalasis@gmail.com>
2018-09-08 14:19:58 +0300

    Refactor Project and Resource modules to utilize new utility methods.

--- usgw/usgw/models/project.py
+++ usgw/usgw/models/project.py
@@ -2,14 +2,12 @@ from usgw.util import success_json
 from usgw.db import get_db
 from bson.objectid import ObjectId
 from model_utilities import to_json_response, from_dict, from_json
+from model_utilities import is_dict_instance, is_dict_instance_strict, get_invalid_field
 
 db = get_db()
 
 
 class Project(object):
-    required_fields = ['user_id', 'title', 'body']
-    fields = required_fields + ['_id']
-
     def __init__(self, user_id, title, body, _id=None):
         # The _id should be the id provided by MongoDB in the _id field.
         self.user_id = user_id
@@ -37,14 +35,11 @@ def get_project_by_id(id):
 
 
 def post_project(request):
-    payload_dictionary = request.get_json()
-    for key in payload_dictionary:
-        if key not in Project.required_fields:
-            return success_json(False, 'POST body contains invalid field ' + str(key))
-    if len(payload_dictionary) < len(Project.required_fields):
-        return success_json(False, 'POST body has too few fields: ' + str(len(payload_dictionary)))
+    payload = request.get_json()
+    if not is_dict_instance_strict(payload, Project):
+        return success_json(False, 'POST body is invalid Project instance.')
     projects = db['projects']
-    projects.insert_one(payload_dictionary)
+    projects.insert_one(payload)
     return success_json(True, 'Request successful.')
 
 
@@ -57,14 +52,14 @@ def delete_project(id):
 
 
 def put_project(id, request):
-    payload_dictionary = request.get_json()
-    for key in payload_dictionary:
-        if key not in Project.required_fields:
-            return success_json(False, 'PUT body contains invalid field ' + str(key))
-    if len(payload_dictionary) == 0:
+    payload = request.get_json()
+    if not is_dict_instance(payload, Project):
+        return success_json(False, 'PUT body contains invalid field: {}'
+                            .format(get_invalid_field(payload, Project)))
+    if len(payload) == 0:
         return success_json(False, 'PUT body is empty.')
     projects = db['projects']
     if projects.find({"_id": ObjectId(id)}).count() == 0:
         return success_json(False, 'No project found with id ' + str(id))
-    document = projects.update_one({'_id': ObjectId(id)}, {'$set': payload_dictionary})
+    document = projects.update_one({'_id': ObjectId(id)}, {'$set': payload})
     return success_json(True, 'Request successful.')
--- usgw/usgw/models/resource.py
+++ usgw/usgw/models/resource.py
@@ -2,14 +2,12 @@ from usgw.util import success_json
 from usgw.db import get_db
 from bson.objectid import ObjectId
 from model_utilities import to_json_response, from_dict, from_json
+from model_utilities import is_dict_instance, is_dict_instance_strict, get_invalid_field
 
 db = get_db()
 
 
 class Resource(object):
-    required_fields = ['user_id', 'title', 'hyperlink', 'tags']
-    fields = required_fields + ['_id']
-
     def __init__(self, user_id, title, hyperlink, tags, _id=None):
         # The _id should be the id provided by MongoDB in the _id field.
         self.user_id = user_id
@@ -38,14 +36,11 @@ def get_resource_by_id(id):
 
 
 def post_resource(request):
-    payload_dictionary = request.get_json()
-    for key in payload_dictionary:
-        if key not in Resource.required_fields:
-            return success_json(False, 'POST body contains invalid field ' + str(key))
-    if len(payload_dictionary) < len(Resource.required_fields):
-        return success_json(False, 'POST body has too few fields: ' + str(len(payload_dictionary)))
+    payload = request.get_json()
+    if not is_dict_instance_strict(payload, Resource):
+        return success_json(False, 'POST body is invalid Resource instance.')
     resources = db['resources']
-    resources.insert_one(payload_dictionary)
+    resources.insert_one(payload)
     return success_json(True, 'Request successful.')
 
 
@@ -58,14 +53,14 @@ def delete_resource(id):
 
 
 def put_resource(id, request):
-    payload_dictionary = request.get_json()
-    for key in payload_dictionary:
-        if key not in Resource.required_fields:
-            return success_json(False, 'PUT body contains invalid field ' + str(key))
-    if len(payload_dictionary) == 0:
+    payload = request.get_json()
+    if not is_dict_instance(payload, Project):
+        return success_json(False, 'PUT body contains invalid field: {}'
+                            .format(get_invalid_field(payload, Resource)))
+    if len(payload) == 0:
         return success_json(False, 'PUT body is empty.')
     resources = db['resources']
     if resources.find({'_id': ObjectId(id)}).count() == 0:
         return success_json(False, 'No resource found with id ' + str(id))
-    document = resources.update_one({'_id': ObjectId(id)}, {'$set': payload_dictionary})
+    document = resources.update_one({'_id': ObjectId(id)}, {'$set': payload})
     return success_json(True, 'Request successful.')
